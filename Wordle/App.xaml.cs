f6b7e75adef93d4ad6bb42e38e3241e131b587d8
SpCoGov <haimiancouna@gmail.com>
2024-02-23 07:58:32 +0800

    移除无用代码

--- Wordle/App.xaml.cs
+++ Wordle/App.xaml.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Threading;
 
 namespace Wordle {
     /// <summary>
@@ -7,48 +7,12 @@ namespace Wordle {
     /// </summary>
     public partial class App : Application {
 
-        public App()
-        {
-            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-            DispatcherUnhandledException += App_DispatcherUnhandledException;
-        }
-
-        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-        {
-            Exception ex = e.ExceptionObject as Exception;
-            HandleException(ex);
-        }
-
-        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
-        {
-            HandleException(e.Exception);
-            e.Handled = true; // Mark the exception as handled
-        }
-
-        private void HandleException(Exception ex)
-        {
-            Console.WriteLine($"Unhandled Exception: {ex.Message}");
-            // 这里可以添加其他处理逻辑，比如记录日志或者弹出错误提示框
-        }
-
         [STAThread]
         public static void Main() {
             Console.WriteLine("Starting...");
-            try {
-                App app = new App();
-                app.InitializeComponent();
-                app.Run();
-            }
-            catch (Exception e) {
-                Console.WriteLine($"An exception occurred: {e.Message}");
-                Console.WriteLine($"Exception details: {e.StackTrace}");
-            }
-            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
-            {
-                Exception e = (Exception)args.ExceptionObject;
-                Console.WriteLine($"Unhandled Exception: {e.Message}");
-                // 这里可以添加其他处理逻辑
-            };
+            App app = new App();
+            app.InitializeComponent();
+            app.Run();
         }
     }
 }
